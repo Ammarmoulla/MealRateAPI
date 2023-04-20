82b1dd1ccfae3ec79436e0a2eccac872b2a4275d
Ammar moulla <ammarturing@gmail.com>
2023-04-20 15:21:00 +0300

    initl commit :tada:

---
+++ api/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Meal, Rating
+# Register your models here.
+class Mealadmin(admin.ModelAdmin):
+    list_filter = ["title", "description"]
+    search_fields = ["title", "description"]
+
+class Ratingadmin(admin.ModelAdmin):
+    list_filter = ["meal", "user"]
+    search_fields = ["meal"]
+
+admin.site.register(Meal, Mealadmin)
+admin.site.register(Rating, Ratingadmin)
---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Meal, Rating
+from .serializers import MealSerializer, RatingSerializer
+from rest_framework import viewsets
+# Create your views here.
+
+class MealViewset(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+class RatingViewset(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
