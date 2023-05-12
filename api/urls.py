909e5a12b0854846b84a349e588cc300021a1a1d
Ammar moulla <ammarturing@gmail.com>
2023-05-13 00:04:35 +0300

    add node for models :hammer:

--- api/urls.py
+++ api/urls.py
@@ -31,5 +31,4 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name="schema"),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path('gql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
